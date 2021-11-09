fa86f1352937f6e579b646ab74b309bda31127f8
Misael Nascimento <nascimentomisael3@gmail.com>
2021-11-08 22:31:29 -0300

    Updates

---
+++ precache-manifest.d7d6b56d7274d360af85e04e7a921358.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31495b7b816c7551d5f4f90647a50067",
+    "url": "/landing-page-react/index.html"
+  },
+  {
+    "revision": "e4ed8b15c10c31ea45c0",
+    "url": "/landing-page-react/static/js/2.920edb4c.chunk.js"
+  },
+  {
+    "revision": "a7f1d584435d180d94e235bd0958329b",
+    "url": "/landing-page-react/static/js/2.920edb4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c78c9a19fdba9d5bdf",
+    "url": "/landing-page-react/static/js/3.e0a1d296.chunk.js"
+  },
+  {
+    "revision": "61f090e1af7728935566",
+    "url": "/landing-page-react/static/js/main.f8572827.chunk.js"
+  },
+  {
+    "revision": "2f25dc35190ad9da54d0",
+    "url": "/landing-page-react/static/js/runtime-main.e866a6aa.js"
+  },
+  {
+    "revision": "ad4bb801efd2f3603a79e68f048cd75f",
+    "url": "/landing-page-react/static/media/banner-pizzaria.ad4bb801.jpg"
+  },
+  {
+    "revision": "a9f1599edad7d976e3b3d91d634f23b5",
+    "url": "/landing-page-react/static/media/kaique.a9f1599e.jpg"
+  },
+  {
+    "revision": "2ad87db4094dc69a479d7c87a2dbcf09",
+    "url": "/landing-page-react/static/media/nathaly.2ad87db4.jpg"
+  },
+  {
+    "revision": "0c794586f353f8a6f83ff3336f42c338",
+    "url": "/landing-page-react/static/media/pizza1.0c794586.jpg"
+  },
+  {
+    "revision": "59430661c610e38a055cca07719c3356",
+    "url": "/landing-page-react/static/media/pizza2.59430661.jpg"
+  },
+  {
+    "revision": "b340b4f401c8e763ffea19f6109b96fa",
+    "url": "/landing-page-react/static/media/pizza3.b340b4f4.jpg"
+  },
+  {
+    "revision": "6cf286b87b2d2e6b88180a378cb9567a",
+    "url": "/landing-page-react/static/media/pizza4.6cf286b8.jpg"
+  },
+  {
+    "revision": "54bd84af57765db2b4cedda80ef37260",
+    "url": "/landing-page-react/static/media/pizza5.54bd84af.jpg"
+  },
+  {
+    "revision": "8d60311e849b48857d22d265a23b2afd",
+    "url": "/landing-page-react/static/media/pizza6.8d60311e.jpg"
+  }
+]);
